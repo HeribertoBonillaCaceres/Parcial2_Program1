7ad218005076e9ce0bb1aa23a9572df2bf952d3f
HeribertoBonillaCaceres <48144500+HeribertoBonillaCaceres@users.noreply.github.com>
2019-03-19 19:53:44 -0600

    Add files via upload

---
+++ ExParcial2_HB/ExParcial2_HB/auxiliar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExParcial2_HB
+{
+    class auxiliar
+    {
+        private int a, b;
+        int i, mult,edad;
+        double div;
+        string nombre;
+
+        public setp1(int a,int b)
+        {
+            this.a=a;
+            this.b=b;
+        }
+
+        public multiplicar()
+        {
+            mult=a*b;
+        }
+    
+        public getp1()
+        {
+             return mult;
+        }
+
+        public setp2(int a,int b)
+        {
+            this.a=a;
+            this.b=b;
+        }
+
+        public dividir()
+        {
+            div=a/b;
+        }
+    
+        public getp2()
+        {
+             return mult;
+        }
+
+        public CapturaNombre()
+        {
+            for(i=1;i<=5;i++)
+            {
+                Console.WriteLine("Digite nombre de alumno: ");
+                nombre=Console.ReadLine();
+                Console.WriteLine("El nombre del alumno "+i+" es "+nombre);
+            }
+            Console.ReadKey();
+        }
+
+        public CapturaEdad()
+        {
+            i=1;
+            do
+            {
+                Console.WriteLine("Digite edad de alumno: ");
+                edad=int.Parse(Console.ReadLine());
+                Console.WriteLine("La edad del alumno "+i+" es "+edad);
+            }while(i<=5);
+            Console.ReadKey();
+        }
+     }
+}
---
+++ ExParcial2_HB/ExParcial2_HB/principal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExParcial2_HB
+{
+    class principal
+    {
+        static void Main(string[] args)
+        {
+            auxiliar Ej1=new auxiliar();
+            auxiliar Ej2=new auxiliar();
+            auxiliar Ej3=new auxiliar();
+            auxiliar Ej4=new auxiliar();
+
+            int a,b,c;
+
+            switch (c)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Ingrese primer numero entero:");
+                        a=int.Parse(Console.ReadLine());
+                        if (a==0) 
+                        {
+                            Console.WriteLine("El producto de 0 por cualquier nùmero es 0");
+                            else
+                            {
+                                Console.WriteLine("Ingrese segundo numero entero:");
+                                b=int.Parse(Console.ReadLine());
+                                Ej3.setp1(a,b);
+                                Console.WriteLine("La multiplicaciòn es: "+getp1());
+                                Console.ReadKey();
+                            }
+                        }
+                    }break;
+                case 2:
+                    {
+                        Console.WriteLine("Ingrese primer numero entero:");
+                        a=int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese segundo numero entero:");
+                        b=int.Parse(Console.ReadLine());
+                        if (b==0) 
+                        {
+                            Console.WriteLine("Error. No se puede dividir entre cero");
+                          else
+                            {  
+                                Ej2.setp2(a,b);
+                                Console.WriteLine("La divisiòn es: "+getp2());
+                            }
+                        }
+                        Console.ReadKey();
+                    }break;
+                case 3:
+                    {
+                       Ej3.CapturaNombre();
+                        Console.ReadKey();
+                    }break;
+                case 4:
+                    {
+                       Ej4.CapturaEdad();
+                       Console.ReadKey();
+                    }break;
+                default:
+                    {
+                        Console.WriteLine("Selecciòn no vàlida");
+                        Console.ReadKey();
+                    }
+
+                    }
+
+            }
+
+        }
+    }
+
